4c760a0737d82cfacc3c2c8b7c601f7b6e32fdb0
joao <joao.ol.dev@gmail.com>
2025-05-23 11:34:53 -0300

    adiciona tarefas ordena por prazo e prioridade

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.time.LocalDate;
 import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
         LinkedList <Tarefa> tarefas = new LinkedList<>();
+
+        Tarefa portifolio = new Tarefa("Criar um portifolio html css e js", LocalDate.of(2025,5,30), 5);
+        Tarefa gameQuizSQL  = new Tarefa("Criar um jogo de perguntas e resopostas", LocalDate.of(2025,5,26), 2);
+        Tarefa sistemadeBiblioteca = new Tarefa("Criar um sistema com cadastro de livros, usuários, empréstimos e devoluções.", LocalDate.of(2025,6,25), 3);
+        Tarefa apiBancaria = new Tarefa("Simular uma API de banco simples que permita: Cadastro de usuários com saldo. Transferências entre contas. Consulta de extrato.", LocalDate.of(2025,6,15), 4);
+
+        tarefas.add(portifolio);
+        tarefas.add(gameQuizSQL);
+        tarefas.add(sistemadeBiblioteca);
+        tarefas.add(apiBancaria);
+
+        Ordenador.ordenarPrazo(tarefas);
+
+        for (Tarefa t : tarefas){
+            System.out.println(t);
+        }
+
+        Ordenador.ordenarPrioridade(tarefas);
+        System.out.println("________________________");
+        for (Tarefa t : tarefas) {
+            System.out.println(t);
+        }
     }
 }
